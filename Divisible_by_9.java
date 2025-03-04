3db1871639d66fe1a1bdacf794489bed20993e78
Karan-SinghRathore <ksinghrathore674@gmail.com>
2025-03-04 23:51:16 +0530

    Largest Number divisible by 9

---
+++ Divisible_by_9.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Divisible_by_9{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        while(n>0){
+            int nine = n%10;
+            sum = sum + nine;
+            n = n /10;
+        }
+        if(sum % 9 == 0){
+            System.out.println("Number is divisible by 9");
+        }
+        else{
+            System.out.println("Number is not divisible by 9");
+        }
+    }
+}
