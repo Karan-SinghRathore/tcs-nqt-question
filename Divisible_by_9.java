b3b005c871d9c1038a1b617edce04f49129778ea
Karan-SinghRathore <ksinghrathore674@gmail.com>
2025-03-04 23:55:17 +0530

    .for

--- Divisible_by_9.java
+++ Divisible_by_9.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Divisible_by_9{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: hello");
+        System.out.println("Enter a number: ");
         int n = sc.nextInt();
         int sum = 0;
         while(n>0){
